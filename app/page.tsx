819bbf728fa449a3d6e4b6a1e6a3c223b174b91f
Apoorv Dwivedi <apoorvd14@gmail.com>
2023-10-27 21:35:40 +0530

    Homepage desktop completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Flex, Heading, Image } from '@chakra-ui/react';
+import { Flex, Heading, Text, Image, Button } from '@chakra-ui/react';
+import Header from './components/Header';
 
 const Page = () => {
   return (
@@ -9,7 +10,7 @@ const Page = () => {
         overflow="hidden"
         position="relative"
         mx="auto"
-        width="min(100vw, 2560px)"
+        width="min(100vw, 177vh)"
         justifyContent="center"
         alignItems="center"
       >
@@ -49,6 +50,7 @@ const Page = () => {
             alt="mandala"
           />
         </Flex>
+        <Header />
         <Flex
           alignItems="center"
           pos="absolute"
@@ -57,9 +59,45 @@ const Page = () => {
           zIndex="2"
           px="5rem"
         >
-          <Heading className="--font-nohemi" fontSize="11rem" fontWeight="600">
-            Innovate<br/> For Good
-          </Heading>
+          <Flex gap="2rem" flexDirection="column">
+            <Heading
+              lineHeight="90%"
+              fontFamily="var(--font-nohemi)"
+              fontSize="11rem"
+              fontWeight="600"
+              letterSpacing="0.3rem"
+            >
+              Innovate
+              <br /> For Good
+            </Heading>
+            <Flex justifyContent="space-between" alignItems="center">
+              <Flex gap="1.5rem">
+                <Image src="/assets/icons/location.svg" alt="location" />
+                <Text fontFamily="var(--font-nohemi)" fontSize="1.6rem">
+                  2 - 5 February, 2024
+                  <br />
+                  Gandhinagar, India
+                </Text>
+              </Flex>
+              <Button
+                color="#fff"
+                px="6rem"
+                py="2rem"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                border="1.5px solid #fff"
+                borderRadius="full"
+                fontFamily="var(--font-nohemi)"
+                fontSize="1.6rem"
+                fontWeight="400"
+                lineHeight="90%"
+                _hover={{ background: '#fff', color: '#0D2129' }}
+              >
+                Register
+              </Button>
+            </Flex>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
