fa6717a64b6edbe706a7fc9d66954ed1e2abbbe8
Apoorv Dwivedi <apoorvd14@gmail.com>
2023-10-28 21:10:22 +0530

    added faq

---
+++ app/faq/page.tsx
@@ -0,0 +1,230 @@
+'use client';
+
+import {
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+} from '@chakra-ui/react';
+import { MinusIcon, AddIcon } from '@chakra-ui/icons';
+import Header from '../components/Header';
+import { useState } from 'react';
+import Footer from '../components/Footer';
+
+const faqs = [
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+  {
+    question: 'Lorem ipsum dolor sit amet consectetur?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+  },
+];
+
+const FAQ = () => {
+  const [currentSection, setCurrentSection] = useState('speakers');
+
+  return (
+    <Flex background="#ffffff" w="100vw" h="100vh">
+      <Flex
+        overflow="hidden"
+        position="relative"
+        mx="auto"
+        width="min(100vw, 200vh)"
+        justifyContent="space-between"
+        alignItems="center"
+        flexDirection="column"
+        height="100vh"
+      >
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          pos="absolute"
+          w="full"
+          h="full"
+          zIndex="1"
+        >
+          <Image
+            animation="clockwiseSpin 30s linear infinite"
+            opacity="0.1"
+            src="/assets/mandala-main-light.svg"
+            h="90%"
+            alt="mandala"
+          />
+          <Image
+            position="absolute"
+            right="-18vh"
+            top="0"
+            animation="antiClockwiseSpin 30s linear infinite"
+            opacity="0.1"
+            src="/assets/mandala-right-light.svg"
+            h="36vh"
+            alt="mandala"
+          />
+          <Image
+            position="absolute"
+            left="-15vh"
+            bottom="-10vh"
+            animation="antiClockwiseSpin 30s linear infinite"
+            opacity="0.1"
+            src="/assets/mandala-left-light.svg"
+            h="40vh"
+            alt="mandala"
+          />
+        </Flex>
+        <Flex
+          position="relative"
+          zIndex="3"
+          flexDirection="column"
+          width="100%"
+          h="full"
+          alignItems="center"
+        >
+          <Header mode="light" position="relative" />
+          <Heading
+            lineHeight="90%"
+            fontFamily="var(--font-nohemi)"
+            fontSize="3rem"
+            color={'#000000'}
+            fontWeight="600"
+            letterSpacing="0.3rem"
+          >
+            FAQs
+          </Heading>
+          <Flex
+            w="full"
+            h="full"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Accordion
+              w="85%"
+              display="grid"
+              gridTemplateColumns="1fr 1fr"
+              rowGap="4rem"
+              columnGap="4rem"
+              allowToggle
+            >
+              {faqs.map((section, index) => {
+                return (
+                  <AccordionItem key={index} border="none">
+                    {({ isExpanded }) => (
+                      <>
+                        <AccordionButton>
+                          <Flex
+                            color="#000"
+                            fontSize="1.2rem"
+                            fontFamily="var(--font-nohemi)"
+                            lineHeight="100%"
+                            w="full"
+                            alignItems="center"
+                            justifyContent="space-between"
+                          >
+                            <Flex alignItems="center" gap="1.5rem">
+                              <Image
+                                src="/assets/faq/icon.svg"
+                                alt="initial icon"
+                                w="1rem"
+                              />
+                              <Text>{section.question}</Text>
+                            </Flex>
+                            {isExpanded ? (
+                              <MinusIcon fontSize="1rem" />
+                            ) : (
+                              <AddIcon fontSize="1rem" />
+                            )}
+                          </Flex>
+                        </AccordionButton>
+                        <AccordionPanel py="2rem" pb={4}>
+                          <Text
+                            fontFamily="var(--font-nohemi)"
+                            fontSize="1rem"
+                            lineHeight="150%"
+                            fontWeight="400"
+                            color="#000"
+                          >
+                            {section.answer}
+                          </Text>
+                        </AccordionPanel>
+                      </>
+                    )}
+                  </AccordionItem>
+                );
+              })}
+            </Accordion>
+          </Flex>
+          <Footer />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default FAQ;
