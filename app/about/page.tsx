712ce847a64ae77c95ba1f87528fd918f532c3d7
Paras Gupta <parasg1999@gmail.com>
2023-11-07 00:48:29 +0530

    fix: about scroll, footer icons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -42,13 +42,109 @@ const metrics = [
   },
 ];
 
-const About = () => {
+const ImageContainer = () => {
   const isMobile = useBreakpointValue({ base: true, lg: false });
 
-  if (typeof window !== "undefined") {
-    var element = document.getElementById("aboutInternalDiv");
-    if (element) element.scrollTop = -element.scrollHeight;
-  }
+  return (
+    <Box
+      maxWidth={{ base: "full", lg: "min(40vw,0.40*1440px)" }}
+      minWidth={"min(40vw,0.40*1440px)"}
+      minHeight={{ base: "21rem", lg: "43rem" }}
+    >
+      <Box className="image-container">
+        <div className="images">
+          <div className="images-slide">
+            <Image
+              alt="1"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/1.svg"
+            />
+            <Image
+              alt="2"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/2.svg"
+            />
+            <Image
+              alt="1"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/1.svg"
+            />
+            <Image
+              alt="2"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/2.svg"
+            />
+          </div>
+        </div>
+      </Box>
+
+      <Box
+        className="image-container relative"
+        marginTop={{ base: "1rem", lg: "2.5rem" }}
+      >
+        <div className="images">
+          <div className="images-slide-rev">
+            <Image
+              alt="1"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/1.svg"
+            />
+            <Image
+              alt="2"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/2.svg"
+            />
+            <Image
+              alt="1"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/1.svg"
+            />
+            <Image
+              alt="2"
+              className="img"
+              height={{ base: "10rem", lg: "20rem" }}
+              display="inline-block"
+              marginX={{ base: "0.5rem", lg: "1rem" }}
+              src="/assets/about/2.svg"
+            />
+          </div>
+        </div>
+        <Box className={`rotating-text${isMobile ? "-mobile" : ""} absolute `}>
+          <Image
+            alt="circular text"
+            animation="clockwiseSpin 15s linear infinite"
+            src="/assets/about/circular-text.svg"
+            width={{ base: "6rem", lg: "10rem" }}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+const About = () => {
+  const isMobile = useBreakpointValue({ base: true, lg: false });
 
   return (
     <Flex background="#ffffff" w="100vw" h="100dvh">
@@ -98,7 +194,7 @@ const About = () => {
         <Header mode="light" position="relative" />
         <Flex
           id="aboutInternalDiv"
-          flexDirection={{ base: "column-reverse", lg: "row" }}
+          flexDirection={{ base: "column", lg: "row" }}
           alignItems="center"
           justifyContent={{ base: "space-between", lg: "center" }}
           flexWrap={{ lg: "wrap" }}
@@ -111,6 +207,7 @@ const About = () => {
           overflow="auto"
           marginTop={{ base: "0rem", lg: "4rem" }}
         >
+          {isMobile && <ImageContainer />}
           <Flex
             flexDirection="column"
             maxWidth={{ base: "full", lg: "min(40vw,0.40*1440px)" }}
@@ -192,103 +289,7 @@ const About = () => {
               ))}
             </Grid>
           </Flex>
-          <Box
-            maxWidth={{ base: "full", lg: "min(40vw,0.40*1440px)" }}
-            minWidth={"min(40vw,0.40*1440px)"}
-          >
-            <Box className="image-container">
-              <div className="images">
-                <div className="images-slide">
-                  <Image
-                    alt="1"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/1.svg"
-                  />
-                  <Image
-                    alt="2"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/2.svg"
-                  />
-                  <Image
-                    alt="1"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/1.svg"
-                  />
-                  <Image
-                    alt="2"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/2.svg"
-                  />
-                </div>
-              </div>
-            </Box>
-
-            <Box
-              className="image-container relative"
-              marginTop={{ base: "1rem", lg: "2.5rem" }}
-            >
-              <div className="images">
-                <div className="images-slide-rev">
-                  <Image
-                    alt="1"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/1.svg"
-                  />
-                  <Image
-                    alt="2"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/2.svg"
-                  />
-                  <Image
-                    alt="1"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/1.svg"
-                  />
-                  <Image
-                    alt="2"
-                    className="img"
-                    width={{ base: "17rem", lg: "34rem" }}
-                    display="inline-block"
-                    marginX={{ base: "0.5rem", lg: "1rem" }}
-                    src="/assets/about/2.svg"
-                  />
-                </div>
-              </div>
-              <Box
-                className={`rotating-text${
-                  isMobile ? "-mobile" : ""
-                } absolute `}
-              >
-                <Image
-                  alt="circular text"
-                  animation="clockwiseSpin 15s linear infinite"
-                  src="/assets/about/circular-text.svg"
-                  width={{ base: "6rem", lg: "10rem" }}
-                />
-              </Box>
-            </Box>
-          </Box>
+          {!isMobile && <ImageContainer />}
         </Flex>
         <Footer mode="light" />
       </Flex>
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,49 +1,58 @@
-'use client';
+"use client";
 
-import { Box, Flex, Heading, Image } from '@chakra-ui/react';
-import Link from 'next/link';
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Footer = (props: { mode: string; marginY?: string }) => {
   return (
     <Flex
       w="full"
-      h={{ base: '20px', lg: '80px' }}
-      px={{ base: '0rem', lg: '5rem' }}
+      h={{ base: "20px", lg: "80px" }}
+      px={{ base: "0rem", lg: "5rem" }}
       alignItems="center"
-      justifyContent={{ base: 'space-evenly', lg: 'flex-end' }}
-      columnGap={{ base: '0.5rem', lg: '4rem' }}
-      marginY={props.marginY || '1.5rem'}
+      justifyContent={{ base: "space-evenly", lg: "flex-end" }}
+      columnGap={{ base: "0.5rem", lg: "4rem" }}
+      marginY={props.marginY || "1.5rem"}
       zIndex={10}
     >
       <Link href="https://x.com/hackthisfall">
-        <Image src={`/assets/footer/x-${props.mode}.svg`} alt="x"></Image>
+        <Image
+          width="2rem"
+          src={`/assets/footer/x-${props.mode}.svg`}
+          alt="x"
+        ></Image>
       </Link>
       <Link href="https://discord.hackthisfall.tech">
         <Image
+          width="2rem"
           src={`/assets/footer/discord-${props.mode}.svg`}
           alt="discord"
         ></Image>
       </Link>
       <Link href="https://instagram.com/hackthisfall">
         <Image
+          width="2rem"
           src={`/assets/footer/instagram-${props.mode}.svg`}
           alt="instagram"
         ></Image>
       </Link>
       <Link href="https://discord.hackthisfall.tech">
         <Image
+          width="2rem"
           src={`/assets/footer/youtube-${props.mode}.svg`}
           alt="youtube"
         ></Image>
       </Link>
       <Link href="mailto:contact@hackthisfall.tech">
         <Image
+          width="2rem"
           src={`/assets/footer/email-${props.mode}.svg`}
           alt="email"
         ></Image>
       </Link>
       <Link href="https://linkedin.com/company/hackthisfall">
         <Image
+          width="2rem"
           src={`/assets/footer/linkedin-${props.mode}.svg`}
           alt="linkedin"
         ></Image>
