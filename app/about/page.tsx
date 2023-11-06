547bb1dede5276e5112c8b58bd8cd9eb0a6f80a1
Paras Gupta <parasg1999@gmail.com>
2023-11-06 23:35:08 +0530

    about page rotating text top

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Box,
@@ -10,43 +10,43 @@ import {
   Image,
   Text,
   useBreakpointValue,
-} from '@chakra-ui/react';
-import Header from '../components/Header';
-import { useEffect, useState } from 'react';
-import Footer from '../components/Footer';
+} from "@chakra-ui/react";
+import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer";
 
 const metrics = [
   {
-    number: '48',
-    text: 'Hours',
+    number: "48",
+    text: "Hours",
   },
   {
-    number: '6000+',
-    text: 'Hackers',
+    number: "6000+",
+    text: "Hackers",
   },
   {
-    number: '40+',
-    text: 'Workshops',
+    number: "40+",
+    text: "Workshops",
   },
   {
-    number: '460+',
-    text: 'Projects',
+    number: "460+",
+    text: "Projects",
   },
   {
-    number: '60+',
-    text: 'Mentors',
+    number: "60+",
+    text: "Mentors",
   },
   {
-    number: '55+',
-    text: 'Countries',
+    number: "55+",
+    text: "Countries",
   },
 ];
 
 const About = () => {
   const isMobile = useBreakpointValue({ base: true, lg: false });
 
-  if (typeof window !== 'undefined') {
-    var element = document.getElementById('aboutInternalDiv');
+  if (typeof window !== "undefined") {
+    var element = document.getElementById("aboutInternalDiv");
     if (element) element.scrollTop = -element.scrollHeight;
   }
 
@@ -98,30 +98,30 @@ const About = () => {
         <Header mode="light" position="relative" />
         <Flex
           id="aboutInternalDiv"
-          flexDirection={{ base: 'column-reverse', lg: 'row' }}
+          flexDirection={{ base: "column-reverse", lg: "row" }}
           alignItems="center"
-          justifyContent={{ base: 'space-between', lg: 'center' }}
-          flexWrap={{ lg: 'wrap' }}
-          px={{ base: '2rem', lg: 0 }}
+          justifyContent={{ base: "space-between", lg: "center" }}
+          flexWrap={{ lg: "wrap" }}
+          px={{ base: "2rem", lg: 0 }}
           w="full"
           h="full"
           zIndex="20"
-          columnGap={{ md: '4rem', '2xl': '5rem' }}
+          columnGap={{ md: "4rem", "2xl": "5rem" }}
           rowGap="0.5rem"
           overflow="auto"
-          marginTop={{ base: '0rem', lg: '4rem' }}
+          marginTop={{ base: "0rem", lg: "4rem" }}
         >
           <Flex
             flexDirection="column"
-            maxWidth={{ base: 'full', lg: 'min(40vw,0.40*1440px)' }}
-            minWidth={'min(40vw,0.40*1440px)'}
+            maxWidth={{ base: "full", lg: "min(40vw,0.40*1440px)" }}
+            minWidth={"min(40vw,0.40*1440px)"}
           >
             <Heading
               fontFamily="var(--font-nohemi)"
               fontWeight="600"
               letterSpacing="0.15rem"
-              fontSize={{ base: '2rem', lg: '4rem' }}
-              marginTop={{ base: '2rem', lg: 'unset' }}
+              fontSize={{ base: "2rem", lg: "4rem" }}
+              marginTop={{ base: "2rem", lg: "unset" }}
             >
               <span className="text-[#000000]">What sets us </span>
               <span className="text-[#F25B2A]">apart</span>
@@ -129,7 +129,7 @@ const About = () => {
             <Box
               color="#000000"
               fontFamily="var(--font-dm-sans)"
-              marginTop={{ base: '0.5rem', lg: '2rem' }}
+              marginTop={{ base: "0.5rem", lg: "2rem" }}
               fontSize="1.25rem"
               textAlign="justify"
             >
@@ -152,7 +152,7 @@ const About = () => {
               className="journey"
               fontSize="1.25rem"
               color="#4A4A4A"
-              marginTop={{ base: '2rem', lg: '4rem' }}
+              marginTop={{ base: "2rem", lg: "4rem" }}
               position="relative"
             >
               <span>Our journey so far</span>
@@ -167,23 +167,23 @@ const About = () => {
             <Grid
               color="#2C2C2C"
               fontFamily="var(--font-nohemi)"
-              templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
+              templateColumns={{ base: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
               rowGap="1rem"
-              columnGap={{ base: '2rem', lg: 'unset' }}
+              columnGap={{ base: "2rem", lg: "unset" }}
               marginTop="2rem"
             >
               {metrics.map((m, i) => (
                 <GridItem key={i}>
                   <Text
                     fontWeight={800}
-                    fontSize={{ base: '1.75rem', lg: '2rem' }}
+                    fontSize={{ base: "1.75rem", lg: "2rem" }}
                     lineHeight="3rem"
                   >
                     {m.number}
                   </Text>
                   <Text
                     fontWeight={500}
-                    fontSize={{ base: '1.4rem', lg: '1.5rem' }}
+                    fontSize={{ base: "1.4rem", lg: "1.5rem" }}
                     lineHeight="1.75rem"
                   >
                     {m.text}
@@ -193,8 +193,8 @@ const About = () => {
             </Grid>
           </Flex>
           <Box
-            maxWidth={{ base: 'full', lg: 'min(40vw,0.40*1440px)' }}
-            minWidth={'min(40vw,0.40*1440px)'}
+            maxWidth={{ base: "full", lg: "min(40vw,0.40*1440px)" }}
+            minWidth={"min(40vw,0.40*1440px)"}
           >
             <Box className="image-container">
               <div className="images">
@@ -202,33 +202,33 @@ const About = () => {
                   <Image
                     alt="1"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/1.svg"
                   />
                   <Image
                     alt="2"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/2.svg"
                   />
                   <Image
                     alt="1"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/1.svg"
                   />
                   <Image
                     alt="2"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/2.svg"
                   />
                 </div>
@@ -237,54 +237,54 @@ const About = () => {
 
             <Box
               className="image-container relative"
-              marginTop={{ base: '1rem', lg: '2.5rem' }}
+              marginTop={{ base: "1rem", lg: "2.5rem" }}
             >
               <div className="images">
                 <div className="images-slide-rev">
                   <Image
                     alt="1"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/1.svg"
                   />
                   <Image
                     alt="2"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/2.svg"
                   />
                   <Image
                     alt="1"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/1.svg"
                   />
                   <Image
                     alt="2"
                     className="img"
-                    width={{ base: '17rem', lg: '34rem' }}
+                    width={{ base: "17rem", lg: "34rem" }}
                     display="inline-block"
-                    marginX={{ base: '0.5rem', lg: '1rem' }}
+                    marginX={{ base: "0.5rem", lg: "1rem" }}
                     src="/assets/about/2.svg"
                   />
                 </div>
               </div>
               <Box
                 className={`rotating-text${
-                  isMobile ? '-mobile' : ''
-                } absolute z-[-10]`}
+                  isMobile ? "-mobile" : ""
+                } absolute `}
               >
                 <Image
                   alt="circular text"
                   animation="clockwiseSpin 15s linear infinite"
                   src="/assets/about/circular-text.svg"
-                  width={{ base: '6rem', lg: '10rem' }}
+                  width={{ base: "6rem", lg: "10rem" }}
                 />
               </Box>
             </Box>
--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import {
   Flex,
@@ -9,87 +9,87 @@ import {
   AccordionItem,
   AccordionButton,
   AccordionPanel,
-} from '@chakra-ui/react';
-import { MinusIcon, AddIcon } from '@chakra-ui/icons';
-import Header from '../components/Header';
-import { useState } from 'react';
-import Footer from '../components/Footer';
+} from "@chakra-ui/react";
+import { MinusIcon, AddIcon } from "@chakra-ui/icons";
+import Header from "../components/Header";
+import { useState } from "react";
+import Footer from "../components/Footer";
 
 const faqs = [
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
   {
-    question: 'Lorem ipsum dolor sit amet consectetur?',
+    question: "Lorem ipsum dolor sit amet consectetur?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.',
+      "Lorem ipsum dolor sit amet consectetur. In augue ipsum tellus ultrices. Ac pharetra ultrices consectetur consequat tellus massa. Nec aliquam cras sagittis duis sed euismod arcu hac. Ornare amet ligula ornare lacus aliquam aenean. Eu lacus imperdiet urna amet congue adipiscing. Faucibus magna nisl ullamcorper in facilisis consequat aliquam.",
   },
 ];
 
 const FAQ = () => {
-  const [currentSection, setCurrentSection] = useState('speakers');
+  const [currentSection, setCurrentSection] = useState("speakers");
 
   return (
     <Flex background="#ffffff" w="100vw" h="100dvh">
@@ -112,7 +112,6 @@ const FAQ = () => {
           zIndex="1"
         >
           <Image
-            animation="clockwiseSpin 30s linear infinite"
             opacity="0.1"
             src="/assets/mandala-main-light.svg"
             h="90%"
@@ -122,7 +121,6 @@ const FAQ = () => {
             position="absolute"
             right="-18vh"
             top="0"
-            animation="antiClockwiseSpin 30s linear infinite"
             opacity="0.1"
             src="/assets/mandala-right-light.svg"
             h="36vh"
@@ -132,7 +130,6 @@ const FAQ = () => {
             position="absolute"
             left="-15vh"
             bottom="-10vh"
-            animation="antiClockwiseSpin 30s linear infinite"
             opacity="0.1"
             src="/assets/mandala-left-light.svg"
             h="40vh"
@@ -152,7 +149,7 @@ const FAQ = () => {
             lineHeight="90%"
             fontFamily="var(--font-nohemi)"
             fontSize="3rem"
-            color={'#000000'}
+            color={"#000000"}
             fontWeight="600"
             letterSpacing="0.3rem"
           >
@@ -161,14 +158,14 @@ const FAQ = () => {
           <Flex
             w="full"
             h="full"
-            mt={{base: "2rem", lg: "4rem"}}
+            mt={{ base: "2rem", lg: "4rem" }}
             justifyContent="center"
             overflowY="scroll"
           >
             <Accordion
               w="85%"
               display="grid"
-              gridTemplateColumns={{base: "1fr", lg:"1fr 1fr"}}
+              gridTemplateColumns={{ base: "1fr", lg: "1fr 1fr" }}
               rowGap="2rem"
               h="fit-content"
               columnGap="4rem"
@@ -179,7 +176,7 @@ const FAQ = () => {
                   <AccordionItem key={index} border="none">
                     {({ isExpanded }) => (
                       <>
-                        <AccordionButton p={{base: "0", md: "1rem"}}>
+                        <AccordionButton p={{ base: "0", md: "1rem" }}>
                           <Flex
                             color="#000"
                             fontSize="1.2rem"
@@ -189,13 +186,18 @@ const FAQ = () => {
                             alignItems="center"
                             justifyContent="space-between"
                           >
-                            <Flex alignItems="center" gap={{base: "1rem", lg: "1.5rem"}}>
+                            <Flex
+                              alignItems="center"
+                              gap={{ base: "1rem", lg: "1.5rem" }}
+                            >
                               <Image
                                 src="/assets/faq/icon.svg"
                                 alt="initial icon"
                                 w="1rem"
                               />
-                              <Text textAlign={{base: "left", lg: "center"}}>{section.question}</Text>
+                              <Text textAlign={{ base: "left", lg: "center" }}>
+                                {section.question}
+                              </Text>
                             </Flex>
                             {isExpanded ? (
                               <MinusIcon fontSize="1rem" />
@@ -222,7 +224,7 @@ const FAQ = () => {
               })}
             </Accordion>
           </Flex>
-          <Footer mode='light' />
+          <Footer mode="light" />
         </Flex>
       </Flex>
     </Flex>
