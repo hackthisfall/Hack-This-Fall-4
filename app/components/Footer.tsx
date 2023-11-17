b2a29fd806b6800de1679eb2d6c33a0b6208a653
Apoorv Dwivedi <apoorvd14@gmail.com>
2023-11-18 00:56:37 +0530

    design fixes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,8 +5,10 @@ import { Box, Flex, Text, Image, Link } from '@chakra-ui/react';
 const Footer = (props: { mode: string; marginY?: string }) => {
   return (
     <Flex
+      flexDir={{ base: 'column', lg: 'row' }}
       w="full"
-      h={{ base: '20px', lg: '80px' }}
+      gap="1rem"
+      h={{ base: '50px', lg: '80px' }}
       px={{ base: '0rem', lg: '5rem' }}
       alignItems="center"
       justifyContent="space-between"
@@ -16,17 +18,19 @@ const Footer = (props: { mode: string; marginY?: string }) => {
       <Link
         lineHeight="90%"
         fontFamily="var(--font-nohemi)"
-        fontSize="1.2rem"
-        color="#FFF"
+        fontSize="1rem"
+        color={props.mode === 'light' ? "#000" : "#FFF"}
         href="https://bit.ly/htf3-sponsor"
         target="_blank"
       >
-        Interested in sponsoring? Reach out to us →
+        Interested in sponsoring? Reach out to us!
       </Link>
       <Flex
+        h={{ base: '20px', lg: '80px' }}
         alignItems="center"
         justifyContent={{ base: 'space-evenly', lg: 'flex-end' }}
         columnGap={{ base: '0.5rem', lg: '4rem' }}
+        w={{"base": "full", lg: "auto"}}
       >
         <Link href="https://x.com/hackthisfall" target="_blank">
           <Image
