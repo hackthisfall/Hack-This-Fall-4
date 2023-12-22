4f030c3fec11414db75ff2e7b0e391f970d972b9
Paras Gupta <parasg1999@gmail.com>
2023-12-23 03:31:58 +0530

    feat: schedule page live

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {
+  Box,
   Button,
   Flex,
   Grid,
@@ -20,28 +21,172 @@ type sessionMappingType = {
   name: string;
   date: Date;
   displayDate: string;
+  platformImage: string;
   link: string;
   image: string;
 };
 
 const sessionMapping: sessionMappingType[] = [
   {
-    title: `Opening Ceremony`,
-    name: `Opening Ceremony`,
-    date: new Date(`2021-10-09T09:00:00`),
-    displayDate: "12th Dec 2023, 12:00 IST",
-    link: `https://www.youtube.com/watch?v=KkT2V4Z2eeE`,
-    image: `/assets/humans/P1.jpeg`,
+    title: "Designing for Diversity",
+    name: "Varsha Saha",
+    date: new Date("2023-12-23T18:00:00"),
+    displayDate: "Dec 23, 2023 · 18:00",
+    platformImage: "/assets/icons/twitter.svg",
+    link: "https://twitter.com/i/spaces/1lPKqbjpDXMGb",
+    image: `/assets/humans/speakers/varsha.jpeg`,
+  },
+  {
+    title: "TBD",
+    name: "Yash Gupta",
+    date: new Date("2023-12-26T17:00:00"),
+    displayDate: "Dec 26, 2023 · 17:00",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/yash.jpeg`,
+  },
+  {
+    title: "Intro to FlutterFire",
+    name: "Ayush Bherwani",
+    date: new Date("2023-12-28T16:00:00"),
+    displayDate: "Dec 28, 2023 · 16:00",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/ayush.png`,
+  },
+  {
+    title: "Learning, Sharing & Building in Public to Skyrocket your Career",
+    name: "Sheetal Jain",
+    date: new Date("2023-12-30T18:00:00"),
+    displayDate: "Dec 30, 2023 · 18:00",
+    platformImage: "/assets/icons/instagram.svg",
+    link: "https://www.instagram.com/hackthisfall",
+    image: `/assets/humans/speakers/sheetal.jpg`,
+  },
+  {
+    title: "Building AR experiences with Meta Spark",
+    name: "Mrunank Pawar",
+    date: new Date("2024-01-02T15:00:00"),
+    displayDate: "Jan 2, 2024 · 15:00",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/mrunank.png`,
+  },
+  {
+    title: "TBD",
+    name: "TBD",
+    date: new Date("2024-01-04T00:00:00"),
+    displayDate: "Jan 4, 2024",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/silhouette.png`,
+  },
+  {
+    title: "TBD",
+    name: "TBD",
+    date: new Date("2024-01-06T00:00:00"),
+    displayDate: "Jan 6, 2024",
+    platformImage: "/assets/icons/twitch.svg",
+    link: "https://hackthisfall.tech/",
+    image: `/assets/humans/speakers/silhouette.png`,
+  },
+  {
+    title: "Build an AI-powered bot with no code",
+    name: "Harshil Agarwal",
+    date: new Date("2024-01-09T17:00:00"),
+    displayDate: "Jan 9, 2024 · 17:00",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/harshil.jpeg`,
+  },
+  {
+    title: "TBD",
+    name: "TBD",
+    date: new Date("2024-01-11T00:00:00"),
+    displayDate: "Jan 11, 2024",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/silhouette.png`,
+  },
+  {
+    title: "TBD",
+    name: "TBD",
+    date: new Date("2024-01-13T18:00:00"),
+    displayDate: "Jan 13, 2024 · 18:00",
+    platformImage: "/assets/icons/twitter.svg",
+    link: "https://twitter.com/hackthisfall",
+    image: `/assets/humans/speakers/silhouette.png`,
+  },
+  {
+    title: "TBD",
+    name: "TBD",
+    date: new Date("2024-01-16T00:00:00"),
+    displayDate: "Jan 16, 2024",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/silhouette.png`,
+  },
+  {
+    title: "GitHub Copilot Workshop",
+    name: "TBD",
+    date: new Date("2024-01-18T00:00:00"),
+    displayDate: "Jan 18, 2024",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/silhouette.png`,
+  },
+  {
+    title: "Hack This Fall AMA",
+    name: "Siddharth Dayalwal",
+    date: new Date("2024-01-20T00:00:00"),
+    displayDate: "Jan 20, 2024",
+    platformImage: "/assets/icons/instagram.svg",
+    link: "https://www.instagram.com/hackthisfall",
+    image: `/assets/humans/speakers/siddharth.jpeg`,
+  },
+  {
+    title: "Super-charge Your Hack with Web APIs",
+    name: "Karl Lingiah",
+    date: new Date("2024-01-23T00:00:00"),
+    displayDate: "Jan 23, 2024",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/karl.jpeg`,
+  },
+  {
+    title: "Orkes Workshop",
+    name: "TBD",
+    date: new Date("2024-01-25T00:00:00"),
+    displayDate: "Jan 25, 2024",
+    platformImage: "/assets/icons/youtube.svg",
+    link: "https://youtube.com/hackthisfall",
+    image: `/assets/humans/speakers/silhouette.png`,
+  },
+  {
+    title: "TBD",
+    name: "TBD",
+    date: new Date("2024-01-27T00:00:00"),
+    displayDate: "Jan 27, 2024",
+    platformImage: "/assets/icons/twitch.svg",
+    link: "https://hackthisfall.tech/",
+    image: `/assets/humans/speakers/silhouette.png`,
   },
 ];
-
-for (let index = 0; index < 8; index++) {
-  sessionMapping.push(sessionMapping[0]);
-}
 
 const Schedule = () => {
   const [currentSection, setCurrentSection] = useState("organizers");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const isDateGreaterThanCurrentDate = (date: Date): boolean => {
+    const currentDate = new Date();
+    return date > currentDate;
+  };
+
+  const isDateMoreThanOneHourBefore = (date: Date): boolean => {
+    const currentDate = new Date();
+    const oneHourBefore = new Date(currentDate.getTime() - 60 * 60 * 1000);
+    return date < oneHourBefore;
+  };
 
   const selectSection = (section: string) => {
     setCurrentSection(section);
@@ -129,24 +274,26 @@ const Schedule = () => {
             rowGap={{ base: "1rem", lg: "1rem" }}
             columnGap={{ base: "1rem", lg: "1rem" }}
             marginTop="2rem"
+            gridAutoRows={{ base: "unset", lg: "1fr" }}
           >
             {sessionMapping.map((s, i) => (
               <Flex
                 key={i}
-                justifyContent="center"
-                paddingLeft={{ base: "0.5rem", lg: "1.5rem" }}
+                justifyContent="space-between"
                 py={{ base: "0.5rem", lg: "1rem" }}
+                paddingLeft={{ base: "0.5rem", lg: "1.5rem" }}
+                paddingRight={{ base: "0.5rem", lg: "2rem" }}
                 borderRadius="0.5rem"
                 flexDirection="column"
                 fontFamily="var(--font-nohemi)"
                 backgroundColor={"#0E252D"}
                 position={"relative"}
-                paddingRight={{ base: "0.5rem", lg: "5rem" }}
                 width={{ base: "100%", lg: "unset" }}
+                height="100%"
               >
-                <Link
-                  href={s.link}
-                  target="_blank"
+                <Box
+                  // href={s.link}
+                  // target="_blank"
                   position={"absolute"}
                   bottom="1rem"
                   right="1rem"
@@ -161,57 +308,81 @@ const Schedule = () => {
                       lg: "1.75rem 1rem",
                     }}
                   />
-                </Link>
-                <Text
-                  color="#F46D24"
-                  fontFamily={"var(--font-dm-sans)"}
-                  fontWeight="600"
-                  letterSpacing="0.05rem"
-                  // fontSize={{ base: "0.5rem", lg: "unset" }}
-                  marginTop="0.5rem"
-                >
-                  {s.name}
-                </Text>
-                {s && s.title && (
+                </Box>
+                <Flex flexDirection="column">
                   <Text
+                    color="#F46D24"
                     fontFamily={"var(--font-dm-sans)"}
-                    color="#ffffff"
-                    opacity="0.5"
-                    letterSpacing="0.06rem"
+                    fontWeight="600"
+                    letterSpacing="0.05rem"
                     // fontSize={{ base: "0.5rem", lg: "unset" }}
+                    marginTop="0.5rem"
                   >
                     {s.title}
                   </Text>
-                )}
-                {s && s.date && (
-                  <Text
-                    fontFamily={"var(--font-dm-sans)"}
-                    color="#ffffff"
-                    letterSpacing="0.06rem"
-                    // fontSize={{ base: "0.5rem", lg: "unset" }}
-                    marginTop={"1rem"}
+                  {s && s.name && (
+                    <Text
+                      fontFamily={"var(--font-dm-sans)"}
+                      color="#ffffff"
+                      opacity="0.5"
+                      letterSpacing="0.06rem"
+                      // fontSize={{ base: "0.5rem", lg: "unset" }}
+                    >
+                      {s.name}
+                    </Text>
+                  )}
+                </Flex>
+                <Flex flexDirection={"column"}>
+                  {s && s.date && (
+                    <Text
+                      fontFamily={"var(--font-dm-sans)"}
+                      color="#ffffff"
+                      letterSpacing="0.06rem"
+                      // fontSize={{ base: "0.5rem", lg: "unset" }}
+                      marginTop={"1rem"}
+                    >
+                      {s.displayDate} IST
+                    </Text>
+                  )}
+                  <Link
+                    href={s.link}
+                    target="_blank"
+                    width="fit-content"
+                    height="fit-content"
+                    className="no-underline"
+                    marginTop={"2rem"}
                   >
-                    {s.displayDate}
-                  </Text>
-                )}
-                <Flex
-                  marginTop={"2rem"}
-                  border={"1px solid white"}
-                  borderRadius={"2rem"}
-                  fontSize={"0.8rem"}
-                  padding="0.5rem 1rem"
-                  width="fit-content"
-                  justify={"center"}
-                  alignItems={"center"}
-                >
-                  <Image src="/assets/icons/twitch.svg" alt="arrow" />
-                  <Text
-                    fontFamily={"var(--font-nohemi)"}
-                    color="#ffffff"
-                    marginLeft={"0.5rem"}
-                  >
-                    Watch Event
-                  </Text>
+                    <Flex
+                      border={"1px solid white"}
+                      borderRadius={"2rem"}
+                      fontSize={"0.8rem"}
+                      padding="0.5rem 1rem"
+                      height="fit-content"
+                      width="fit-content"
+                      justify={"center"}
+                      alignItems={"center"}
+                      color="#ffffff"
+                      _hover={{
+                        background: "#fff",
+                        color: "#0D2129",
+                      }}
+                      cursor="pointer"
+                      className="schedule-link"
+                    >
+                      <Image src={s.platformImage} alt="arrow" />
+                      <Text
+                        fontFamily={"var(--font-nohemi)"}
+                        marginLeft={"0.5rem"}
+                        marginTop={"2px"}
+                      >
+                        {isDateGreaterThanCurrentDate(s.date)
+                          ? `Upcoming`
+                          : isDateMoreThanOneHourBefore(s.date)
+                          ? `Watch Now`
+                          : `Live Now`}
+                      </Text>
+                    </Flex>
+                  </Link>
                 </Flex>
               </Flex>
             ))}
